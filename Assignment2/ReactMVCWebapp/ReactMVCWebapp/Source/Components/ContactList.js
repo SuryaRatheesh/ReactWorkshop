89504b49159ace3a5e35d67d81e58fc608d0d5ed
SuryaRatheesh <65178099+SuryaRatheesh@users.noreply.github.com>
2020-05-24 23:28:25 +0530

    Add files via upload

---
+++ Assignment2/ReactMVCWebapp/ReactMVCWebapp/Source/Components/ContactList.js
@@ -0,0 +1,39 @@
+﻿import React, { Component } from 'react';
+
+
+const contacts = [
+    {
+        name: "Shinto Thomas",
+        email: "sthomas@suyati.com"
+    },
+    {
+        name: "Praphul",
+        email: "pnangeelil@suyati.com"
+    },
+    {
+        name: "Shamnad V A",
+        email: "sabdul@suyati.com"
+    }
+];
+
+
+const ContactList = ({ contacts }) => {
+    return (
+        <table className="contacts-table">
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                {contacts.map((contact, i) => (
+                    <tr key={i}>
+                        <td>{contact.name}</td>
+                        <td>{contact.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default ContactList;
